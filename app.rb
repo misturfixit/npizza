9d0bf1139cdf78ba5a16736a6666ce03e7f327d3
misturfixit <japullins420@gmail.com>
2017-08-21 15:09:32 -0400

    should wurk now

--- app.rb
+++ app.rb
@@ -5,7 +5,23 @@ get '/' do
   erb :stuff
 end
 post '/stuff' do
-	session[:flesh] = params[:meat]
+	session[:meat] = params[:meat]
+	session[:vegs] = params[:veggies]
+	session[:cheeze] = params[:cheeze]
+	session[:crust] = params[:crust]
 	redirect '/confrm' 
 end
 #######################################################
+get '/confrm' do
+erb :confrm, locals:{meat:session[:meat],vegs:session[:vegs],
+cheeze:session[:cheeze],crust:session[:crust]}
+end
+post '/chozen' do
+	choze = params[:pickem]
+	session[:checked] = params[:pickem]
+	redirect '/gotit'
+end	
+#######################################################
+get '/gotit' do
+	erb :results, locals:{checked:session[:checked]}
+end	
