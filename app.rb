ac85961317c7c49dd8b6a47ea66791b484d1868d
misturfixit <japullins420@gmail.com>
2017-08-22 17:27:14 -0400

    wurkin w/o a counter....yee-ha

--- app.rb
+++ app.rb
@@ -9,19 +9,27 @@ post '/stuff' do
 	session[:vegs] = params[:veggies]
 	session[:cheeze] = params[:cheeze]
 	session[:crust] = params[:crust]
+	session[:delivery] = params[:delivery]
 	redirect '/confrm' 
 end
 #######################################################
 get '/confrm' do
-erb :confrm, locals:{meat:session[:meat],vegs:session[:vegs],
-cheeze:session[:cheeze],crust:session[:crust]}
+erb :confrm, locals:{meat:session[:meat],
+		vegs:session[:vegs],
+		cheeze:session[:cheeze],###need to put sauce in also
+		crust:session[:crust],
+		delivery:session[:delivery]
+		}
 end
 post '/chozen' do
 	choze = params[:pickem]
+	session[:address] = params[:address]
 	session[:checked] = params[:pickem]
 	redirect '/gotit'
 end	
 #######################################################
 get '/gotit' do
-	erb :results, locals:{checked:session[:checked]}
+	erb :results, locals:{checked:session[:checked],
+			address:session[:address]	
+			}
 end	
