be751190361e3bc11104a34b1c54d9139e055842
misturfixit <japullins420@gmail.com>
2017-08-28 14:22:20 -0400

    wheweeee think i got  all parts wurkin!!!

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ post '/stuff' do
 		session[:crust] = params[:crust]
 		session[:delivery] = params[:delivery]
 		session[:size] = params[:size]
+		session[:cost] = params[:cost]
 	redirect '/confrm' 
 end
 #######################################################
@@ -20,10 +21,11 @@ get '/confrm' do
 		@params = params
 		print @params
 	erb :confrm, locals:{meat:session[:meat],
-				vegs:session[:vegs],
-					cheeze:session[:cheeze],
-					crust:session[:crust],
-				size:session[:size],
+			vegs:session[:vegs],
+			cheeze:session[:cheeze],
+			crust:session[:crust],
+			size:session[:size],
+			cost:session[:cost],
 			delivery:session[:delivery]
 			}
 end
@@ -34,28 +36,34 @@ post '/chozen' do
 		#session[:pickem] = []
 		session[:checked] = params[:pickem]
 		session[:address] = params[:address]
-		session[:cost] = params[:cost]
-		session[:pickem] = checked.values
+		session[:pickem] = session[:checked].values
 	redirect '/gotit'
 end	
 #######################################################
 get '/gotit' do
 		@params = params
 		print @params
+		session[:cost] = params[:cost]
 	erb :results, locals:{pickem:session[:pickem],
 			address:session[:address], cost:session[:cost]	
 			}
 end	
 #######################################################
 post '/gotit' do
-		pizzaz = {}
-		pizzaz << session[:pickem]
+		if session[:pizzas] == nil  
+		 session[:pizzas] = []
+		 session[:pizzas] << session[:pickem].values
+		else
+		  session[:pizzas] << session[:pickem].values
+		end   
 		@params = params
 		print @params
 	redirect '/finale'
 end
 #######################################################
 get '/finale' do
+	 "hullloo wuurrllld"
+	erb :final, locals:{pizzas:session[pizzas]
 
-##erb :final, locals:{*-*:session[:*-*]}
+	} 
 end	
