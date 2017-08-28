fa3d6b9c5b540ea7a790179accb42a7b09eb3e39
misturfixit <japullins420@gmail.com>
2017-08-28 10:31:05 -0400

    m'kay finally found the block in my price func. checking if the hash I'm making to iterate, store, and retrieve data is doing so...

--- app.rb
+++ app.rb
@@ -5,47 +5,57 @@ get '/' do
   erb :stuff
 end
 post '/stuff' do
-	@params = params
-	print @params
-	session[:meat] = params[:meat]
-	session[:vegs] = params[:veggies]
-	session[:cheeze] = params[:cheeze]
-	session[:crust] = params[:crust]
-	session[:delivery] = params[:delivery]
-	session[:size] = params[:size]
+		@params = params
+		print @params
+		session[:meat] = params[:meat]
+		session[:vegs] = params[:veggies]
+		session[:cheeze] = params[:cheeze]
+		session[:crust] = params[:crust]
+		session[:delivery] = params[:delivery]
+		session[:size] = params[:size]
 	redirect '/confrm' 
 end
 #######################################################
 get '/confrm' do
-	@params = params
-	print @params
-erb :confrm, locals:{meat:session[:meat],
-		vegs:session[:vegs],
-		cheeze:session[:cheeze],
-		crust:session[:crust],
-		size:session[:size],
-		delivery:session[:delivery]
-		}
+		@params = params
+		print @params
+	erb :confrm, locals:{meat:session[:meat],
+				vegs:session[:vegs],
+					cheeze:session[:cheeze],
+					crust:session[:crust],
+				size:session[:size],
+			delivery:session[:delivery]
+			}
 end
 post '/chozen' do
-	@params = params
-	print @params
-	checked = params[:pickem]
-	session[:address] = params[:address]
-	session[:pickem] = checked.values
+		@params = params
+		print @params
+		session[:checked] = []
+		#session[:pickem] = []
+		session[:checked] = params[:pickem]
+		session[:address] = params[:address]
+		session[:cost] = params[:cost]
+		session[:pickem] = checked.values
 	redirect '/gotit'
 end	
 #######################################################
 get '/gotit' do
-	@params = params
-	print @params
+		@params = params
+		print @params
 	erb :results, locals:{pickem:session[:pickem],
-			address:session[:address]	
+			address:session[:address], cost:session[:cost]	
 			}
 end	
+#######################################################
 post '/gotit' do
-	pizza1 = {}
-	pizza1 << session[:pickem]
-	@params = params
-	print @params
+		pizzaz = {}
+		pizzaz << session[:pickem]
+		@params = params
+		print @params
+	redirect '/finale'
 end
+#######################################################
+get '/finale' do
+
+##erb :final, locals:{*-*:session[:*-*]}
+end	
